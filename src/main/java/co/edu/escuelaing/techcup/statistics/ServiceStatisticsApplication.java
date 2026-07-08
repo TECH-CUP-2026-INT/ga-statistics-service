0c50e66102f5d0314b1ad76f33b0295cdb12d26e
julianizz18 <904juliancorredor@gmail.com>
2026-07-07 23:46:26 -0500

    Realizacion del repo

---
+++ src/main/java/co/edu/escuelaing/techcup/statistics/ServiceStatisticsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.techcup.statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceStatisticsApplication.class, args);
+	}
+
+}
